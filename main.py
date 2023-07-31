7357d09fdb4371eba7fd9975c7ba58e4cfd49edc
Manoel-Lidio-Gosalan <devgosalan@gmail.com>
2023-07-30 22:12:37 -0400

    atualização

--- main.py
+++ main.py
@@ -2,11 +2,26 @@
 Websoket: serve como ia de comunicação entre o backend e o frontend
 jquery: serve para fazer requisições js
 
+site : cdnjs.com
+
+pip install python-socketio flask-socketio simple-websocket
+
 """
 
 from flask import render_template, app, Flask
+from flask_socketio import SocketIO, send
+
 
 app = Flask(__name__)
+
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+# funcionalidade de enviar mensagens
+
+@socketio.on('message')
+def gerenciar_mensagem(mensagem):
+    """função que gerencia as mensagens enviadas pelo usuário"""
+    send(mensagem, broadcast=True)
 
 @app.route('/')
 
@@ -14,4 +29,4 @@ def home():
     """função que renderiza a página inicial"""
     return render_template('home.html')
 
-app.run()
+socketio.run(app, host='localhost', debug=True)
